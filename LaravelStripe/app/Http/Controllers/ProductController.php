7490f72385293de0dbff34abdc68e2768282a61d
Mizanur Rahman <mizanur1326@gmail.com>
2024-03-10 13:44:31 +0600

    send order in db & make payment

--- LaravelStripe/app/Http/Controllers/ProductController.php
+++ LaravelStripe/app/Http/Controllers/ProductController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
+use App\Models\OrderDetails;
 use App\Models\Product;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -98,10 +100,49 @@ class ProductController extends Controller
     
     public function Order(Request $request): RedirectResponse
     {
+
+        if(session('cart')){
+            $carts = session('cart');
+            $total = 0;
+            $customerID = 1;
+            foreach($carts as $id=>$details){
+                echo $id . '<br>';
+                $total += $details['price'] * $details['quantity'];
+            }
+        }
+
+        $order_data = [
+            'totalamount' => $total,
+            'customer_id' => $customerID,
+            'coupon_discount' => '0',
+            'payment_method' => 'stripe'
+
+        ];
+        $order_id = Order::insertGetId($order_data); 
+        echo $order_id . '<br>';
+        echo $total . '<br>';
+
+
+        foreach($carts as $id=>$details){        
+        $order_details_data = [
+            'order_id' => $order_id,
+            'product_id' => $id,
+            'product_qty' => $details['quantity'],
+            'product_price' => $details['price'],
+            'product_subtotal' => $details['quantity'] * $details['price'],
+            'product_discount' => '0',
+        ];
+        OrderDetails::insert($order_details_data);
+    }
+
+        // dd($carts);
+        
+
         Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
       
         Stripe\Charge::create ([
-                "amount" => 10 * 100,
+                // "amount" => 10 * 100,
+                "amount" => $total * 100,
                 "currency" => "usd",
                 "source" => $request->stripeToken,
                 "description" => "Test payment from itsolutionstuff.com." 
---
+++ LaravelStripe/app/Models/Order.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'totalamount', 
+        'customer_id', 
+        'coupon_discount', 
+        'status', 
+        'payment_method'
+    ];
+}
